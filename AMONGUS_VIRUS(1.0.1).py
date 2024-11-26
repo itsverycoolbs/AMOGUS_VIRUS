e1d0a8796d91f277586ade9f654eccea65de73cb
itsverycoolbs <ogruet@isbremen.de>
2024-11-26 13:11:53 +0100

    Add files via upload

---
+++ AMONGUS_VIRUS(1.0.1).py
@@ -0,0 +1,16 @@
+import ctypes
+
+def show_windows_error():
+    """
+    Function to show a Windows 10-style error message using ctypes.
+    """
+    ctypes.windll.user32.MessageBoxW(
+        None,
+        "UR DEVICE IS DONE FOR",
+        "AMOGUS_VIRUS",
+        0x10 | 0x0
+    )
+
+if __name__ == "__main__":
+    while True:
+        show_windows_error()
